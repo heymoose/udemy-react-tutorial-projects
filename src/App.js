d70c08b86fa542e57b98460fa96938b55dcffcb7
heymoose <marcusanovak@gmail.com>
2018-12-14 19:27:23 -0700

    Add input with change event and reflect input in p element

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    inputText: ''
+  }
+
+  textUpdateHandler = (event) => {
+    this.setState({ inputText: event.target.value })
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>
-          Hey
-        </h1>
+        <input 
+          type="text"
+          onChange={(event) => this.textUpdateHandler(event)}>
+        </input>
+        <p>{this.state.inputText}</p>
       </div>
     );
   }
