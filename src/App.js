8598511c1a05861bb4049f4fbfc3553c089e9065
heymoose <marcusanovak@gmail.com>
2018-12-17 14:37:22 -0700

    Dynamically assign classes to paragraph depending on lenght of list

--- src/App.js
+++ src/App.js
@@ -68,10 +68,20 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
+    const classes = [];
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>Hello there, this is the react app</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
           style={style} 
           onClick={this.togglePersonsHandler}>Toggle Persons
