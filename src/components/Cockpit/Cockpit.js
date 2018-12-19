514ba8dbfd6646a225195165b669d85707be52ee
heymoose <marcusanovak@gmail.com>
2018-12-19 10:27:49 -0700

    Use higher order components to remove redundant div when returning component jsx elements

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -3,10 +3,10 @@ import style from './Cockpit.css';
 
 const cockpit = (props) => {
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = style.Button;
 
     if (props.showPersons) {
-        btnClass = style.red;
+        btnClass = [style.red, style.Button].join(' ');
     }
 
     if (props.persons.length <= 2) {
@@ -17,15 +17,17 @@ const cockpit = (props) => {
       assignedClasses.push(style.bold);
     }
 
+    // Aux is a higher order component that is useful for returning the children elements without a wrapping div or using an array
+    // Using the empty tags <> and </> called fragments, does the same thing, and is a new React 16.2 feature that automatically does what Aux does
     return (
-        <div className={style.Cockpit}>
+        <>
             <h1>{props.appTitle}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons
             </button>
-        </div>
+        </>
     );
 }
 
