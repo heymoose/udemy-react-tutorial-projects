ea59e976f9c9ed12288755be4d0d00ac433cf825
heymoose <marcusanovak@gmail.com>
2018-12-21 12:25:15 -0700

    Add BurgerBuilder component, add Layout.css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './Layout.css';
 
 const layout = (props) => (
     <>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={style.Content}>
             {props.children}
         </main>
     </>
