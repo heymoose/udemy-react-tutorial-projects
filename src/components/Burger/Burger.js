1e9eee315a04a5f8c2bdafd07212dc57cf9d053d
heymoose <marcusanovak@gmail.com>
2018-12-21 21:42:35 -0700

    Flatten ingredients array to determine if there are none added, insert message if this is true

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,20 @@ import style from './Burger.css'
 import Ingredient from './Ingredient/Ingredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])]
                 .map((_, i) => {
                     return <Ingredient key={igKey + i} ingredientType={igKey} />
                 });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
 
     return (
         <div className={style.Burger}>
