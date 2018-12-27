d0df696cf64a3eeb91f8d6411f7f34a891308968
heymoose <marcusanovak@gmail.com>
2018-12-27 10:51:10 -0700

    Optimizations and wrapping up

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import style from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <>
-        <Backdrop 
-            show={props.show}
-            clicked={props.modalClosed} />
-        <div
-            className={style.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </>
-);
+class Modal extends Component {
+    
+    // Only update the modal component if it is shown
+    // This optimization prevents the OrderSummary component from
+    // getting updated even when it's info is not on the screen
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    render() {
+        return (
+            <>
+                <Backdrop 
+                    show={this.props.show}
+                    clicked={this.props.modalClosed} />
+                <div
+                    className={style.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </>
+        );
+    }
+}
+
+export default Modal;
